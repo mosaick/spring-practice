4a3bbc2d355b46d5f1fe1f7be73f0e2839f52947
mosaick <mosaick2@gmail.com>
2018-08-15 09:59:11 +0900

    servlet practice

---
+++ src/main/java/servlet/SampleServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class SampleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head><title>Servlet</title></head>");
+        out.println("<body><h1>Sample Servlet</h1>");
+        out.println("<p>Welcome to Sample Servlet page! Hello world</p>");
+        out.println("</body></html>");
+    }
+}
